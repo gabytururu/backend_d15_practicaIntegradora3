cb97a9e28f72441feac1ab02a55053d1bc689106
gabriela tururu <gabriela.aguayaire@gmail.com>
2024-07-26 10:01:01 -0600

    Desafio15_limpieza basica

--- src/public/js/password.js
+++ src/public/js/password.js
@@ -43,39 +43,9 @@ emailVerifyButton.addEventListener('click',async(e)=>{
             },7000)
         } 
     }catch(error){
-        console.log('fetch error at password reset screen:',error)
+        console.log('fetch error at password email verification screen:',error)
     }
    
 })
 
 
-//--------------------------------
- // const response = await fetch('/api/sessions/password',{
-    //     method: 'POST',
-    //     headers:{
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body:JSON.stringify(email)
-    // });
-    // if (!response.ok) {
-    //   throw new Error('Network response was not ok');
-    // }
-    // OJO AQUI CON EL REQ.DTO DEL API/SESSIONS/CUSRRENT SE ESTA JODIENDO EL ENVIO DE CURRENT TUVE Q QUITAR DTO PARA Q OPERARA BIEN REVISAR
-    // const password = await response.json();
-    // console.log({password})
-    // const currentUserEmail = await currentUser.payload.email;
-   // return currentUserEmail
-
-   //------------------------------------------
-
-
-
-// async function verifyEmail() {
-//    // const response = await fetch('/api/sessions/password');
-//     // if (!response.ok) {
-//     //   throw new Error('Network response was not ok');
-//     // }
-//     // const currentUser = await response.json();
-//     // const currentUserEmail = await currentUser.payload.email;
-//     // return currentUserEmail
-// }
